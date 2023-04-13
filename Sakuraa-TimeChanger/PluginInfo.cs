22b55050b776c381e0f470d311cfee672948c392
HuddleX <73008150+SakuraaDevelopment@users.noreply.github.com>
2023-04-12 19:52:47 -0700

    starting with Graicc's template :3
    starting with `github.com/Graicc/GorillaTagModTemplate` :3

---
+++ Sakuraa-TimeChanger/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Sakuraa_TimeChanger
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.sakuraa.gorillatag.sakuraa-timechanger";
+		public const string Name = "Sakuraa_TimeChanger";
+		public const string Version = "1.0.0";
+	}
+}
