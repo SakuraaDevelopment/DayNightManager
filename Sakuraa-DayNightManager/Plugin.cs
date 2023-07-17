899eda4caeb2aa3c47bdc4ddc8fa6e8312e684ce
SakuraaGT <73008150+SakuraaDevelopment@users.noreply.github.com>
2023-07-16 21:55:48 -0700

    Update Plugin.cs

--- Sakuraa-DayNightManager/Plugin.cs
+++ Sakuraa-DayNightManager/Plugin.cs
@@ -18,13 +18,8 @@ namespace Sakuraa_DayNightManager
 
         void Update()
         {
-            // Find the BetterDayNight GameObject in the scene
             GameObject betterDayNight = GameObject.Find("BetterDayNight");
-
-            // Get the BetterDayNightManager component
             BetterDayNightManager betterDayNightManager = betterDayNight.GetComponent<BetterDayNightManager>();
-
-            // Use reflection to access the private baseSeconds field
             var baseSecondsField = typeof(BetterDayNightManager).GetField("baseSeconds", BindingFlags.NonPublic | BindingFlags.Instance);
             if (baseSecondsField != null)
             {
